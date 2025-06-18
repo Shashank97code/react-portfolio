d7ff8903bd49439bd2c9f704eb47d8938a36d09c
shashank-at-719892440678 <shashank.mm@logicarts.in>
2025-06-18 18:54:50 +0530

    feat: added about section impoted in App compoenent

---
+++ src/components/About.tsx
@@ -0,0 +1,28 @@
+function About() {
+  return (
+    <section id="about" className="bg-gray-100 py-8">
+      <div className="max-w-5xl mx-auto px-4">
+        <h2 className="text-3xl font-bold text-gray-900 mb-5 text-center">
+          About Me
+        </h2>
+        <div className="bg-white p-6 rounded-lg shadow-md">
+          <p className="text-gray-700 mb-4">
+            Hello! I'm Shashank, a passionate MERN Stack Developer with 3 years
+            of experience building responsive, scalable, and user-friendly web
+            applications. I specialize in React.js, Node.js, MongoDB, and
+            Express.js, and I'm also AWS Certified, enabling me to build and
+            deploy applications with cloud efficiency.
+          </p>
+          <p className="text-gray-700">
+            When I'm not coding, you’ll find me exploring new technologies,
+            reading tech blogs, or hiking. I’m always eager to learn and stay
+            ahead in the industry by expanding my skills across both frontend
+            and backend development.
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default About;
