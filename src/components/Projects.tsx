03941bb28ce3aa79dc8e15361a32b9c600c69156
Shashank-MM <vbkh472@gmail.com>
2025-07-09 01:16:54 +0530

    feat: add new project 'Todo App' and update project images

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,9 +1,10 @@
 import Project1Image from "../assets/react-gmap-ui-preview-with-vehicle-marker.png";
 import PortfolioImage from "../assets/portfolio-preview.png";
-import LogiSaas from "../assets/LogiSaas-preview.png";
+import LogiSaas from "../assets/logiSaas-project-preview.png";
 import DashboardAdminView from "../assets/student-enter-key.png";
 import github from "../assets/svg/github.svg";
 import { useState } from "react";
+import TodoAppPreview from "../assets/todo-app-preview.png";
 
 const Projects = () => {
   const [siteType] = useState("personal");
@@ -15,7 +16,110 @@ const Projects = () => {
           Projects
         </h2>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-          {/* Project 1  */}
+          {/* Project 1 : ToDo */}
+          <div className="bg-gray-100 rounded-lg overflow-hidden shadow-md hover:shadow-2xl transition-shadow duration-300">
+            <img
+              src={TodoAppPreview}
+              alt="React + Google Map UI"
+              className="w-full h-48 object-cover transition-transform duration-300 hover:scale-105"
+            />
+            <div className="p-4">
+              <h3 className="text-lg font-semibold mb-2">
+                <div className="flex justify-between items-center">
+                  <div className="text-gray-900">React ToDo App - Todozi</div>
+                  <div className="flex space-x-2">
+                    <a
+                      title="View Source Code on GitHub"
+                      href="https://github.com/Shashank97code/todozi"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      className="text-sm hover:color-blue-600 hover:bg-blue-200  p-0.5 rounded "
+                    >
+                      <img src={github} alt="github Logo" className="w-4 h-4" />
+                    </a>
+                    <a
+                      title="View Live Demo"
+                      href="https://ornate-empanada-3d786a.netlify.app/"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      className="text-sm hover:color-blue-600 hover:bg-blue-200  p-0.5 rounded "
+                    >
+                      🔗
+                    </a>
+                  </div>
+                </div>
+              </h3>
+              <p className="text-gray-700 mb-3">
+                A simple and intuitive ToDo app built with React, allowing users
+                to create, complete, and manage tasks efficiently. The app
+                features a clean UI and responsive design.
+              </p>
+              <div className="flex flex-wrap gap-2 mb-3">
+                <span className="bg-blue-100 text-blue-800 text-xs px-2 py-1 rounded inline-block w-auto min-w-[60px] text-center">
+                  React
+                </span>
+                <span className="bg-blue-100 text-blue-800 text-xs px-2 py-1 rounded inline-block w-auto min-w-[90px] text-center">
+                  React Hooks
+                </span>
+                <span className="bg-blue-100 text-blue-800 text-xs px-2 py-1 rounded inline-block w-auto min-w-[60px] text-center">
+                  Tailwind
+                </span>
+              </div>
+            </div>
+          </div>
+          {/* Project 2  : portfolio*/}
+          <div className="bg-gray-100 rounded-lg overflow-hidden shadow-md hover:shadow-2xl transition-shadow duration-300">
+            <img
+              src={PortfolioImage}
+              alt="Portfolio Website"
+              className="w-full h-48 object-cover transition-transform duration-300 hover:scale-105"
+            />
+            <div className="p-4">
+              <h3 className="text-lg font-semibold text-gray-900 mb-2">
+                <div className="flex justify-between items-center">
+                  <div> Personal Portfolio Website</div>
+                  <div className="flex space-x-2">
+                    <a
+                      title="View Source Code on GitHub"
+                      href="https://github.com/Shashank97code/react-portfolio"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      className="text-sm hover:color-blue-600 hover:bg-blue-200  p-0.5 rounded "
+                    >
+                      <img src={github} alt="github Logo" className="w-4 h-4" />
+                    </a>
+                    {siteType === "personal" ? (
+                      <a
+                        title="Currently Viewing"
+                        href="#"
+                        className="text-sm text-gray-400 cursor-not-allowed p-0.5 rounded"
+                        tabIndex={-1}
+                        aria-disabled="true"
+                      >
+                        👁️
+                      </a>
+                    ) : null}
+                  </div>
+                </div>
+              </h3>
+              <p className="text-gray-700 mb-3">
+                A personal portfolio website to showcase projects, skills, and
+                experience, built with React and Firebase.
+              </p>
+              <div className="flex space-x-2">
+                <span className="bg-blue-100 text-blue-800 text-xs px-2 py-1 rounded">
+                  React
+                </span>
+                <span className="bg-blue-100 text-blue-800 text-xs px-2 py-1 rounded">
+                  Tailwind
+                </span>
+                <span className="bg-blue-100 text-blue-800 text-xs px-2 py-1 rounded">
+                  Components based
+                </span>
+              </div>
+            </div>
+          </div>
+          {/* Project 3  : react-maps*/}
           <div className="bg-gray-100 rounded-lg overflow-hidden shadow-md hover:shadow-2xl transition-shadow duration-300">
             <img
               src={Project1Image}
@@ -68,59 +172,7 @@ const Projects = () => {
               </div>
             </div>
           </div>
-          {/* Project 2 */}
-          <div className="bg-gray-100 rounded-lg overflow-hidden shadow-md hover:shadow-2xl transition-shadow duration-300">
-            <img
-              src={PortfolioImage}
-              alt="Portfolio Website"
-              className="w-full h-48 object-cover transition-transform duration-300 hover:scale-105"
-            />
-            <div className="p-4">
-              <h3 className="text-lg font-semibold text-gray-900 mb-2">
-                <div className="flex justify-between items-center">
-                  <div> Personal Portfolio Website</div>
-                  <div className="flex space-x-2">
-                    <a
-                      title="View Source Code on GitHub"
-                      href="https://github.com/Shashank97code/react-portfolio"
-                      target="_blank"
-                      rel="noopener noreferrer"
-                      className="text-sm hover:color-blue-600 hover:bg-blue-200  p-0.5 rounded "
-                    >
-                      <img src={github} alt="github Logo" className="w-4 h-4" />
-                    </a>
-                    {siteType === "personal" ? (
-                      <a
-                        title="Currently Viewing"
-                        href="#"
-                        className="text-sm text-gray-400 cursor-not-allowed p-0.5 rounded"
-                        tabIndex={-1}
-                        aria-disabled="true"
-                      >
-                        👁️
-                      </a>
-                    ) : null}
-                  </div>
-                </div>
-              </h3>
-              <p className="text-gray-700 mb-3">
-                A personal portfolio website to showcase projects, skills, and
-                experience, built with React and Firebase.
-              </p>
-              <div className="flex space-x-2">
-                <span className="bg-blue-100 text-blue-800 text-xs px-2 py-1 rounded">
-                  React
-                </span>
-                <span className="bg-blue-100 text-blue-800 text-xs px-2 py-1 rounded">
-                  Tailwind
-                </span>
-                <span className="bg-blue-100 text-blue-800 text-xs px-2 py-1 rounded">
-                  Components based
-                </span>
-              </div>
-            </div>
-          </div>
-          {/* Project 3 - */}
+          {/* Project 4 - LogiSaas*/}
           <div className="bg-gray-100 rounded-lg overflow-hidden shadow-md hover:shadow-2xl transition-shadow duration-300">
             <img
               src={LogiSaas}
@@ -188,7 +240,7 @@ const Projects = () => {
               </div>
             </div>
           </div>
-          {/* Project 3 - */}
+          {/* Project 5 - Vb KHSS-KHXR */}
           <div className="bg-gray-100 rounded-lg overflow-hidden shadow-md hover:shadow-2xl">
             <img
               src={DashboardAdminView}
